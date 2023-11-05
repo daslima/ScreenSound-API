5b3878370357b8c8980eb4ceaa10ce5d3a2608d4
David <davidlimait@outlook.com>
2023-11-05 19:15:40 -0300

    Projeto Inicial

---
+++ ScreenSound.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using (var client = new HttpClient())
+{
+    try
+    {
+        string baseURL = "https://guilhermeonrails.github.io/api-csharp-songs/songs.json";
+        string resp =  await client.GetStringAsync(baseURL);
+        Console.WriteLine(resp);
+    }
+    catch (Exception error)
+    {
+        Console.WriteLine($"Have a problem {error.Message.ToLower()}");
+    }
+
+}
